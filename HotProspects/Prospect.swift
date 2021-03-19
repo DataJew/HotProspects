e018352416eb42820e896f8361d6c0d75200fbb0
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-19 13:09:40 -0400

    feat: Added Class for Prospect and an Observable Object class for Prospects
    The Prospect class was added in order to hold three data varibles and one id by UUID generated type. The Prospects class holds a Published varible of people that holds an empty array of those Prospects.

--- HotProspects/Prospect.swift
+++ HotProspects/Prospect.swift
@@ -5,4 +5,19 @@
 //  Created by Matthew Garlington on 3/19/21.
 //
 
-import Foundation
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    var isContacted = false
+}
+
+class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+    
+    init() {
+        self.people = []
+    }
+}
