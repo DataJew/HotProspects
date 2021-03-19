056869ded700d223cbe4b42723557bb37aac6a4e
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-19 13:07:04 -0400

    feat: Added Button and Enviroment Object
    The button is set in the upper right corner of the screen that has a qr scanner button and when the button is pressed, the action adds a person count to the Enivorment Object added of Prospects.

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -11,8 +11,10 @@ struct ProspectsView: View {
     enum FilterType {
         case none, contacted, uncontacted
     }
+    
+    @EnvironmentObject var prospects: Prospects
     let filter: FilterType
-   
+    
     var title: String {
         switch filter {
         case .none:
@@ -23,11 +25,20 @@ struct ProspectsView: View {
             return "Uncontacted People"
         }
     }
-   
+    
     var body: some View {
         NavigationView {
-            Text("Hello wrold")
+            Text("People: \(prospects.people.count)")
                 .navigationTitle(title)
+                .navigationBarItems(trailing: Button(action: {
+                    let prospect = Prospect()
+                    prospect.name = "Matthew Garlington"
+                    prospect.emailAddress = "SOMatthewG@gmail.com"
+                    self.prospects.people.append(prospect)
+                }, label: {
+                    Image(systemName: "qrcode.viewfinder")
+                    Text("Scan")
+                }))
         }
     }
 }
