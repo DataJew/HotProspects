cd8f583a625567e97f97927acadfa905fd83c590
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-19 10:23:34 -0400

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Matthew Garlington on 3/19/21.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
